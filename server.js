a52944d1be6ddfd2859c1884571e1c0f3d347682
EPIC <hiyer@epicchannel.com>
2020-01-23 16:53:44 +0530

    This is Demo

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello EPIC ??\n');
+  res.send('This is demo ??\n');
 });
 
 app.listen(PORT, HOST);
